b1157e2491e9b4433dfb0e0c25a6ecf036f82d4f
rnazb <r.nazbat@gmail.com>
2021-08-30 10:45:35 +0800

    Add loading icon to marketplace products render

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -47,31 +47,24 @@ const Products = () => {
           <Link to={`/product/${productItem.id}`}>
             <Card className="cardHighlight">
               {
-                isLoading ?
-                  <BeatLoader css={override} />
+                productItem.images?.length > 0 ?
+                  <Card.Img src={productItem.images[0].url} />
                   :
-                  <>
-                    {
-                      productItem.images?.length > 0 ?
-                        <Card.Img src={productItem.images[0].url} />
-                        :
-                        <Card.Img src="https://res.cloudinary.com/daddiqd6y/image/upload/v1626241614/Tinda/xjfxzr1ytbtquyt6bvyi.png" />
-                    }
-                    <ProductItem
-                      id={productItem.id}
-                      name={productItem.name}
-                      price={productItem.price.toFixed(2)}
-                    />
-                    <Card.Footer>
-                      <div className="ratings">
-                        <RatingView ratingValue={Math.floor(productItem.averageRating)} />
-                        <span>({
-                          productItem.averageRating.toFixed(1)
-                        })</span>
-                      </div>
-                    </Card.Footer>
-                  </>
+                  <Card.Img src="https://res.cloudinary.com/daddiqd6y/image/upload/v1626241614/Tinda/xjfxzr1ytbtquyt6bvyi.png" />
               }
+              <ProductItem
+                id={productItem.id}
+                name={productItem.name}
+                price={productItem.price.toFixed(2)}
+              />
+              <Card.Footer>
+                <div className="ratings">
+                  <RatingView ratingValue={Math.floor(productItem.averageRating)} />
+                  <span>({
+                    productItem.averageRating.toFixed(1)
+                  })</span>
+                </div>
+              </Card.Footer>
             </Card>
           </Link>
         </div>
@@ -82,11 +75,18 @@ const Products = () => {
   return (
     <Row>
       {
-        productsList.length !== 0
-          ?
-          productsList
+        isLoading ?
+          <BeatLoader css={override} />
           :
-          <p>No products found...</p>
+          <>
+            {
+              productsList.length !== 0
+                ?
+                productsList
+                :
+                <p>No products found...</p>
+            }
+          </>
       }
     </Row>
   );
